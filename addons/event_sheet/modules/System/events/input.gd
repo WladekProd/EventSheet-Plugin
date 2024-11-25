8358f2ed1307a05b5365e17a3d71d5a8f8239f36
Wladek <seoghost976@gmail.com>
2024-11-25 02:35:46 +0200

    new

--- addons/event_sheet/modules/System/events/input.gd
+++
@@ -1,9 +0,0 @@
-
-static func params() -> Dictionary:
-	return { }
-
-static func get_template(params: Dictionary = params()) -> String:
-	return """func _input() -> void:""".format({ })
-
-static func get_info(params: Dictionary = params()) -> String:
-	return """Input""".format({ })
--- addons/event_sheet/source/elements/blank_body/content/blank_actions.gd
+++
@@ -1,4 +0,0 @@
-@tool
-extends VBoxContainer
-
-@onready var add_action_button: Button = $AddActionItem/AddAction
